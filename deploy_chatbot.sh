b5ec3f8b6101f992c8e8fcbed3a9c060bfd4bd71
Siu-Yin Lee <70337168+qwgyuz86@users.noreply.github.com>
2025-01-04 18:30:41 -0700

    Update deploy_chatbot.sh

--- deploy_chatbot.sh
+++ deploy_chatbot.sh
@@ -31,31 +31,3 @@ docker run -d --network chatbot-net --name chatbot-app -p 7860:7860 \
 echo "Chatbot logs:"
 docker logs -f chatbot-app
 
-
-# #!/bin/bash
-
-# # Step 1: Start Ollama Container
-# echo "Starting Ollama container..."
-# docker run -d -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama
-
-# # Wait for Ollama to start
-# echo "Waiting for Ollama to initialize..."
-# sleep 10
-
-# # Step 2: Load and Run Llama Model in Ollama
-# echo "Loading Llama model in Ollama..."
-# docker exec -it ollama ollama pull wangshenzhi/llama3-8b-chinese-chat-ollama-q4
-
-# # Step 3: Build the Chatbot Docker Image
-# echo "Building chatbot Docker image..."
-# docker build -t chatbot-app .
-
-# # Step 4: Run the Chatbot Container
-# echo "Running chatbot container..."
-# docker run -d --name chatbot-app -p 7860:7860 -e EMBED_DEVICE_CHOICE="cpu" chatbot-app
-# # docker run -d --name chatbot-app -p 11434:11434 -e EMBED_DEVICE_CHOICE="cpu" chatbot-app
-# # docker run -d --name chatbot-app --network="host" -e EMBED_DEVICE_CHOICE="cpu" chatbot-app
-
-# # Step 5: Display Logs
-# echo "Chatbot logs:"
-# docker logs -f chatbot-app
